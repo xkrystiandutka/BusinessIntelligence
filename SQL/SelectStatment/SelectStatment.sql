5085c273aab0384527bea9438f2b1065bdd79ad0
Krystian <Krystian.Dutkait@gmail.com>
2022-09-17 18:48:47 +0200

    Using like

--- SQL/SelectStatment/SelectStatment.sql
+++ SQL/SelectStatment/SelectStatment.sql
@@ -94,3 +94,30 @@ SELECT * FROM employees WHERE first_name IN( 'Denis', 'Elvis');
 # Extract all records from the ‘employees’ table, aside from those with employees named John, Mark, or Jacob.
 
 SELECT * FROM employees WHERE first_name NOT IN ('John' , 'Mark', 'Jacob');
+
+# Using LIKE - NOT LIKE
+
+SELECT * FROM employees WHERE first_name like ('Mar_');
+SELECT * FROM employees WHERE first_name not like ('&MAR&');
+
+# Working with the “employees” table, use the LIKE operator to select the data about all individuals, whose first name starts with “Mark”; specify that the name can be succeeded by any sequence of characters.
+
+SELECT * FROM employees WHERE first_name LIKE('Mark%');
+
+# Retrieve a list with all employees who have been hired in the year 2000.
+
+SELECT * FROM employees WHERE hire_date LIKE ('%2000%');
+
+# Retrieve a list with all employees whose employee number is written with 5 characters, and starts with “1000”. 
+
+ SELECT * FROM employees WHERE emp_no LIKE ('1000_');
+ 
+# Wildcard characters
+
+# Extract all individuals from the ‘employees’ table whose first name contains “Jack”.
+
+ SELECT * FROM employees WHERE first_name LIKE ('%JACK%');
+
+# Once you have done that, extract another list containing the names of employees that do not contain “Jack”.
+ 
+SELECT * FROM employees WHERE first_name NOT LIKE ('%Jack%'); 
