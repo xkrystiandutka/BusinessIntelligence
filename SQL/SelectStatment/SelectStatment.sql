2eff1b2a04f828e37910cbcad5db58dc9986c42d
Krystian <Krystian.Dutkait@gmail.com>
2022-09-17 18:59:05 +0200

    Using between - and & in null / is not null

--- SQL/SelectStatment/SelectStatment.sql
+++ SQL/SelectStatment/SelectStatment.sql
@@ -121,3 +121,32 @@ SELECT * FROM employees WHERE hire_date LIKE ('%2000%');
 # Once you have done that, extract another list containing the names of employees that do not contain “Jack”.
  
 SELECT * FROM employees WHERE first_name NOT LIKE ('%Jack%'); 
+
+#  BETWEEN - AND
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date BETWEEN '1990-01-01' AND '2000-01-01';
+    
+# Select all the information from the “salaries” table regarding contracts from 66,000 to 70,000 dollars per year.
+
+SELECT * FROM salaries WHERE salary BETWEEN 66000 AND 70000;
+
+# Retrieve a list with all individuals whose employee number is not between ‘10004’ and ‘10012’.
+
+SELECT * FROM employees WHERE emp_no NOT BETWEEN '10004' AND '10012';
+
+# Select the names of all departments with numbers between ‘d003’ and ‘d006’. 
+
+SELECT dept_name FROM departments WHERE dept_no BETWEEN 'd003' AND 'd006';
+
+#IS NOT NULL - IS NULL
+
+SELECT * FROM employees where first_name is null;
+
+# Select the names of all departments whose department number value is not null.
+
+SELECT dept_name FROM departments WHERE dept_no IS NOT NULL;
