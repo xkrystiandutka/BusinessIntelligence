763965ba6ede6e41c82c8a23807adaa64afc0f7c
Krystian <Krystian.Dutkait@gmail.com>
2022-09-18 16:03:33 +0200

    Aggregate functions & select distinct

--- SQL/SelectStatment/SelectStatment.sql
+++ SQL/SelectStatment/SelectStatment.sql
@@ -150,3 +150,67 @@ SELECT * FROM employees where first_name is null;
 # Select the names of all departments whose department number value is not null.
 
 SELECT dept_name FROM departments WHERE dept_no IS NOT NULL;
+
+# Using Other Comparison Operators
+
+ SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name != 'Mark';
+ 
+  SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date > '2000-01-01';
+    
+ 
+  SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date <= '1985-02-01';
+    
+# Retrieve a list with data about all female employees who were hired in the year 2000 or after.
+# Hint: If you solve the task correctly, SQL should return 7 rows.
+
+select * from employees where gender = 'f' and hire_date > '2000-01-01';
+
+# Extract a list with all employees’ salaries higher than $150,000 per annum.
+
+SELECT * FROM salaries WHERE salary > 150000;
+
+#  SELECT DISTINCT
+
+SELECT distinct
+    gender
+FROM
+    employees;
+    
+# Obtain a list with all different “hire dates” from the “employees” table.
+
+# Expand this list and click on “Limit to 1000 rows”. This way you will set the limit of output rows displayed back to the default of 1000.
+
+SELECT DISTINCT hire_date FROM employees;
+
+#  Getting to Know Aggregate Functions
+
+SELECT 
+    COUNT(distinct first_name)
+FROM
+    employees;
+
+select * from employees where first_name is null;
+
+# How many annual contracts with a value higher than or equal to $100,000 have been registered in the salaries table?
+
+SELECT COUNT(*) FROM salaries WHERE salary >= 100000;
+
+# How many managers do we have in the “employees” database? Use the star symbol (*) in your code to solve this exercise.
+
+SELECT COUNT(*) FROM dept_manager;
+
