25ea13b5e4f9ff8c7cd3c7aeef92b9738dbbc821
Krystian <Krystian.Dutkait@gmail.com>
2022-09-18 18:05:23 +0200

    Having vs where & limit

--- SQL/SelectStatment/SelectStatment.sql
+++ SQL/SelectStatment/SelectStatment.sql
@@ -273,3 +273,29 @@ FROM
 GROUP BY emp_no
 HAVING AVG(salary) > 120000
 ORDER BY emp_no;
+
+# WHERE vs HAVING
+
+SELECT 
+    first_name, COUNT(first_name) AS names_count
+FROM
+    employees
+WHERE
+    hire_date > '1999-01-01'
+GROUP BY first_name
+HAVING COUNT(first_name) < 200
+ORDER BY first_name DESC
+limit 100;
+
+# Select the employee numbers of all individuals who have signed more than 1 contract after the 1st of January 2000.
+# Hint: To solve this exercise, use the “dept_emp” table.
+
+SELECT emp_no FROM dept_emp WHERE from_date > '2000-01-01' GROUP BY emp_no HAVING COUNT(from_date) > 1 ORDER BY emp_no;
+
+# Limit
+
+select * from salaries order by emp_no desc limit 10;
+
+# Select the first 100 rows from the ‘dept_emp’ table.
+
+select * from dept_emp limit 100;
