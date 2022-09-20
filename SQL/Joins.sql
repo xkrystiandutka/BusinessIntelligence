6d86ae5c2dbd95ece4374968270c1eba09a86902
Krystian <Krystian.Dutkait@gmail.com>
2022-09-20 21:20:05 +0200

    Duplicate rows

--- SQL/Joins.sql
+++ SQL/Joins.sql
@@ -19,6 +19,7 @@ FROM
     dept_manager_dup m
         INNER JOIN
     departments_dup d ON m.dept_no = d.dept_no
+group by m.emp_no
 ORDER BY m.dept_no;
 
 # Extract a list containing information about all managers’ employee number, first and last name, department number, and hire date. 
@@ -29,3 +30,49 @@ FROM
     employees e
         JOIN
     dept_manager dm ON e.emp_no = dm.emp_no;
+    
+# Using Joins
+
+SELECT 
+    m.dept_no, m.emp_no, m.from_date, m.to_date, d.dept_name
+FROM
+    dept_manager_dup m
+        INNER JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+ORDER BY m.dept_no;
+
+/*
+Select m.dept_no, m.emp_no, d.dept_name
+FROM dept_manager_dup m
+JOIN 
+departments_dup d ON d.dept_no = m.dept_no
+ORDER BY d.dept_no;
+*/
+
+# Duplicate Rows
+
+#duplicate records
+
+INSERT INTO dept_manager_dup
+VALUES('110228', 'd003', '1992-03-21', '9999-01-01');
+
+INSERT INTO departments_dup
+VALUES('d009', 'Customer Service');
+
+#check table
+
+SELECT 
+    *
+FROM
+    dept_manager_dup
+ORDER BY dept_no ASC;
+
+SELECT 
+    *
+FROM
+    departments_dup
+ORDER BY dept_no ASC;
+
+
+
+
