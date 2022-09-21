38bba4e31b83263ebdfc53e70601a38f2b0fa487
Krystian <Krystian.Dutkait@gmail.com>
2022-09-21 21:54:00 +0200

    Left joins task

--- SQL/Joins.sql
+++ SQL/Joins.sql
@@ -121,3 +121,21 @@ FROM
     departments_dup d ON m.dept_no = d.dept_no
     where dept_name is null
 ORDER BY m.dept_no;
+
+#Join the 'employees' and the 'dept_manager' tables to return a subset of all the employees whose last name is Markovitch. See if the output contains a manager with that name.  
+
+#Hint: Create an output containing information corresponding to the following fields: ‘emp_no’, ‘first_name’, ‘last_name’, ‘dept_no’, ‘from_date’. Order by 'dept_no' descending, and then by 'emp_no'.
+
+SELECT
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    dm.from_date
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+WHERE
+    e.last_name = 'Markovitch'
+ORDER BY dm.dept_no DESC, e.emp_no;
