d07cc06fae9dc0d6359df522a1c2173b1953cca9
Krystian <Krystian.Dutkait@gmail.com>
2022-09-22 22:04:43 +0200

    New and old join syntax

--- SQL/Joins.sql
+++ SQL/Joins.sql
@@ -159,3 +159,48 @@ FROM
 	dept_manager_dup m ON m.dept_no = d.dept_no
 ORDER BY dept_no;
 
+# Differences between the New and the Old Join Syntax
+
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+		JOIN
+	departments_dup d ON m.dept_no = d.dept_no
+ORDER BY m.dept_no;
+
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m,
+    departments_dup d
+WHERE 
+	m.dept_no = d.dept_no
+ORDER BY m.dept_no;
+
+# Extract a list containing information about all managers’ employee number, first and last name, department number, and hire date. Use the old type of join syntax to obtain the result.
+
+SELECT
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    e.hire_date
+FROM
+    employees e,
+    dept_manager dm
+WHERE
+    e.emp_no = dm.emp_no;
+
+# New Join Syntax:
+
+SELECT
+	e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    e.hire_date
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no; 
