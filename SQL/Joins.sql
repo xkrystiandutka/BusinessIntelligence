5b315b0a2428d46acb9469346c520a4012a5450e
Krystian <Krystian.Dutkait@gmail.com>
2022-09-22 22:39:12 +0200

    Cross join

--- SQL/Joins.sql
+++ SQL/Joins.sql
@@ -224,3 +224,50 @@ WHERE
     first_name = 'Margareta'
         AND last_name = 'Markovitch'
 ORDER BY e.emp_no;   
+
+# CROSS JOIN
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm
+        CROSS JOIN
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm,
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm
+        JOIN
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    dm.*, d.*
+FROM
+    departments d
+        CROSS JOIN
+    dept_manager dm
+WHERE
+	d.dept_no <> dm.dept_no
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    dm.*, e.*
+FROM
+    departments d
+        CROSS JOIN
+    dept_manager dm
+		JOIN
+	employees e ON dm.emp_no = e.emp_no
+WHERE
+	d.dept_no <> dm.dept_no
+ORDER BY dm.emp_no , d.dept_no;
