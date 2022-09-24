6d735793af7bdf8ea1a02ece32cdeef8d768b75e
Krystian <Krystian.Dutkait@gmail.com>
2022-09-24 11:31:02 +0200

    Sql subqueries with exists

--- SQL/Subqueries.sql
+++ SQL/Subqueries.sql
@@ -14,3 +14,64 @@ WHERE
             dm.emp_no
         FROM
             dept_manager dm);
+            
+SELECT 
+    dm.emp_no
+FROM
+    dept_manager dm;
+    
+# Extract the information about all department managers who were hired between the 1st of January 1990 and the 1st of January 1995.
+
+SELECT
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no IN (SELECT
+            emp_no
+        FROM
+            employees
+        WHERE
+            hire_date BETWEEN '1990-01-01' AND '1995-01-01');
+            
+# SQL Subqueries with EXISTS-NOT EXISTS Embedded Inside WHERE
+
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no);
+            
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no
+order by emp_no);
+
+# Select the entire information for all employees whose job title is “Assistant Engineer”. 
+
+SELECT
+    *
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT
+            *
+        FROM
+            titles t
+        WHERE
+            t.emp_no = e.emp_no
+                AND title = 'Assistant Engineer');
