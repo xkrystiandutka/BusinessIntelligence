900ad6cb72104a79da56e5367f61e10e4863574d
Krystian <Krystian.Dutkait@gmail.com>
2022-09-25 14:20:51 +0200

    Stored procedures with an input parameter

--- SQL/StoredRoutines.sql
+++ SQL/StoredRoutines.sql
@@ -44,3 +44,28 @@ CALL employees.avg_salary;
 CALL employees.avg_salary();
 
 
+# Stored Procedures with an Input Parameter
+
+USE employees;
+
+DROP procedure IF exists emp_salary;
+
+DELIMITER $$
+USE employees $$
+create procedure emp_salary(IN p_emp_no INTEGER)
+BEGIN
+SELECT
+	e.first_name, e.last_name, AVG(s.salary)
+FROM
+	employees e
+		JOIN
+	salaries s  ON e.emp_no = s.emp_no
+where
+	e.emp_no = p_emp_no;
+END$$
+
+DELIMITER ;
+
+call employees.emp_salary(11300);
+
+call emp_salary(11300);
