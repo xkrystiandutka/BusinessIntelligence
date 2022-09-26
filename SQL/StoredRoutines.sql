d9bdb42150cc79fbfebbc1dc67766975b541b54c
Krystian <Krystian.Dutkait@gmail.com>
2022-09-26 16:43:29 +0200

    Sql variables

--- SQL/StoredRoutines.sql
+++ SQL/StoredRoutines.sql
@@ -115,3 +115,23 @@ END$$
 
 DELIMITER ;
 
+
+# SQL Variables
+
+set @v_avg_salary = 0;
+call employees.emp_avg_salary_out(11300, @v_avg_salary);
+
+Select @v_avg_salary;
+select @p_avg_salary;
+
+# Create a variable, called ‘v_emp_no’, where you will store the output of the procedure you created in the last exercise.
+
+# Call the same procedure, inserting the values ‘Aruna’ and ‘Journel’ as a first and last name respectively.
+
+# Finally, select the obtained output.
+
+SET @v_emp_no = 0;
+
+CALL emp_info('Aruna', 'Journel', @v_emp_no);
+
+SELECT @v_emp_no;
