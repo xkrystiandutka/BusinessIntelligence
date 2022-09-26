92f5f823ad91b41249484030100ecc3f86ed164e
Krystian <Krystian.Dutkait@gmail.com>
2022-09-27 00:50:06 +0200

    The ROW_NUMBER() ranking window function

---
+++ SQL/WindowFunctions.sql
@@ -0,0 +1,37 @@
+# Window Functions
+
+# The ROW_NUMBER() Ranking Window Function and the Relevant MySQL Syntax
+
+use employees;
+
+SELECT 
+    emp_no,
+    salary,
+    ROW_NUMBER() OVER(partition by emp_nu order by salary desc) as row_num
+FROM
+    salaries;
+    
+    # Exercise #1 :
+
+# Write a query that upon execution, assigns a row number to all managers we have information for in the "employees" database (regardless of their department).
+
+# Let the numbering disregard the department the managers have worked in. Also, let it start from the value of 1. Assign that value to the manager with the lowest employee number.
+
+SELECT
+    emp_no,
+    dept_no,
+    ROW_NUMBER() OVER (ORDER BY emp_no) AS row_num
+FROM
+dept_manager;
+
+# Exercise #2:
+
+# Write a query that upon execution, assigns a sequential number for each employee number registered in the "employees" table. Partition the data by the employee's first name and order it by their last name in ascending order (for each partition).
+
+SELECT
+emp_no,
+first_name,
+last_name,
+ROW_NUMBER() OVER (PARTITION BY first_name ORDER BY last_name) AS row_num
+FROM
+employees;
