de868ec7cc1f3ed13256dff97ac3edfd6dd1c03d
Krystian <Krystian.Dutkait@gmail.com>
2022-09-26 16:59:41 +0200

    User-defined functions

--- SQL/StoredRoutines.sql
+++ SQL/StoredRoutines.sql
@@ -135,3 +135,66 @@ SET @v_emp_no = 0;
 CALL emp_info('Aruna', 'Journel', @v_emp_no);
 
 SELECT @v_emp_no;
+
+# User-Defined Functions in MySQL
+
+USE employees;
+DROP FUNCTION IF exists f_emp_avg_salary;
+
+DELIMITER $$
+create function f_emp_avg_salary (p_emp_no integer) returns decimal (10,2)
+deterministic no sql reads sql data
+begin
+declare v_avg_salary decimal(10,2);
+
+SELECT 
+    AVG(s.salary)
+INTO v_avg_salary FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.emp_no = p_emp_no;
+return v_avg_salary;
+END$$
+
+select f_emp_avg_salary(11300);
+
+# Create a function called ‘emp_info’ that takes for parameters the first and last name of an employee, and returns the salary from the newest contract of that employee.
+
+# Hint: In the BEGIN-END block of this program, you need to declare and use two variables – v_max_from_date that will be of the DATE type, and v_salary, that will be of the DECIMAL (10,2) type.
+
+# Finally, select this function.
+
+DELIMITER $$
+
+CREATE FUNCTION emp_info(p_first_name varchar(255), p_last_name varchar(255)) RETURNS decimal(10,2)
+
+BEGIN
+    DECLARE v_max_from_date date;
+    DECLARE v_salary decimal(10,2);
+                SELECT
+    MAX(from_date)
+INTO v_max_from_date FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.first_name = p_first_name
+        AND e.last_name = p_last_name;
+                SELECT
+    s.salary
+INTO v_salary FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.first_name = p_first_name
+        AND e.last_name = p_last_name
+        AND s.from_date = v_max_from_date;
+RETURN v_salary;
+END$$
+
+DELIMITER ;
+
+SELECT EMP_INFO('Aruna', 'Journel');
